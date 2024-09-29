1c098b9d608ed4ba4fc0a604d26c03a6e28f80c5
Adit Ghosh <blubrdpirts@gmail.com>
2024-09-29 18:53:43 +0530

    Datatypes and Typecasting

--- Main.java
+++ Main.java
@@ -14,5 +14,34 @@ public class Main {
 
                System.out.println("Hello "+name);
 
+               //Datatypes and Typecasting
+
+               int myNum = 5;               // Integer (whole number)
+               float myFloatNum = 5.99f;    // Floating point number
+               char myLetter = 'D';         // Character
+               boolean myBool = true;       // Boolean
+               String myText = "Hello";     // String
+               
+               //Printing
+               System.out.println(myNum);
+               System.out.println(myFloatNum);
+               System.out.println(myLetter);
+               System.out.println(myBool);
+               System.out.println(myText);
+
+
+               //TypeCasting
+
+               int myIntg = 9; //Wide casting
+               double myDobuble = myIntg; // Automatic casting: int to double
+
+               System.out.println(myIntg);      // Outputs 9
+               System.out.println(myDobuble);   // Outputs 9.0
+
+               double myDouble = 9.78d; //Narrow casting
+               int myInt = (int) myDouble; // Manual casting: double to int
+
+               System.out.println(myDouble);   // Outputs 9.78
+               System.out.println(myInt);      // Outputs 9
     }
 }
