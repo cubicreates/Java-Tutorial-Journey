71e73a02f8eadc240e95f4407b0811cd844709f3
Adit Ghosh <blubrdpirts@gmail.com>
2024-09-29 18:35:38 +0530

    Printing Statements and User Inputs

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner; // Module for user input
+
 public class Main {
     public static void main(String[] args) {
-        
+               // Printing Statements
+               System.out.println("Hello World!"); 
+            
+               //Getting the scanner ready
+               Scanner scan = new Scanner(System.in);
+
+               //Usage
+               System.out.println("What is your name?");
+               String name = scan.nextLine();
+
+               System.out.println("Hello "+name);
+
     }
 }
